c4bd15fb4938bb25adf4b18a94f70a95087f29b2
Chellton Almeida <chelltonoliveiraalmeida@gmail.com>
2019-03-06 13:10:41 -0300

    Add files via upload

---
+++ GRAVACAO_DE_DADOS_NA_EEPROM/GRAVACAO_DE_DADOS_NA_EEPROM.ino
@@ -0,0 +1,24 @@
+#include <EEPROM.h>
+
+//Numero a ser armazenado
+int numero = 123;
+int endereco = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  //Escrita
+  Serial.print("Gravando numero na memoria EEPROM : ");
+  Serial.println(numero);
+  EEPROM.write(endereco, numero);
+  delay(2000);
+  //Leitura
+  Serial.print("Lendo numero da memoria EEPROM : ");
+  Serial.println(EEPROM.read(endereco));
+  while(1)
+  {}
+}
---
+++ LEITURA_EEPROM/LEITURA_EEPROM.ino
@@ -0,0 +1,18 @@
+#include <EEPROM.h>
+
+//Numero a ser armazenado
+int numero = 123;
+int endereco = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  Serial.print("Lendo numero da memoria EEPROM : ");
+  Serial.println(EEPROM.read(endereco1));
+  while(1)
+  {}
+}
